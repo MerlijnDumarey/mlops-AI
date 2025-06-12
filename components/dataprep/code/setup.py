465f265c21b9910588c796eedf4566edcee1a132
Pollet Daymon <daymon.pollet@student.howest.be>
2025-06-12 09:18:15 +0200

    hardcoded absolutely needed requirements in the setup file

--- components/dataprep/code/setup.py
+++ components/dataprep/code/setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="mlops_cleaning",
     version="0.1.0",
     py_modules=["mlops_cleaning"],
-    install_requires=requirements,
+    install_requires=[
+        "h5py",
+        "pandas",
+        "numpy",
+        "scikit-learn" 
+    ],
 )
